6c18f3fa1a33fe93d13d3cac1627848b38b2b77b
mamtadalal <dmamta8@gmail.com>
2016-03-08 16:22:15 +0530

    Update countrycontrollers.js

--- countrycontrollers.js
+++ countrycontrollers.js
@@ -1,6 +1,9 @@
 var countryApp = angular.module('CinemaSite', []);
+console.log("here");
 countryApp.controller('CountryCtrl', function ($scope, $http) {
+  console.log("1");
 $http.get('countries.json').success(function(data) {
+console.log("2");
 $scope.countries = data;
   });
 });
